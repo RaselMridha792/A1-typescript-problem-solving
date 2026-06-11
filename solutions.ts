00103ffc4d7fb42e1e422c92af30c53b92d3b02b
RaselMridha792 <raselmridha792@gmail.com>
2026-06-11 23:50:29 +0600

    solve problem 6

--- solutions.ts
+++ solutions.ts
@@ -34,3 +34,28 @@ interface Book {
 function toggleReadStatus(book: Book): Book & { isRead: boolean } {
   return { ...book, isRead: true };
 }
+
+
+// Problem 6 solutions
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
