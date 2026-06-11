45aa35b7e7b25ab5793ee1f9bc8574c7492ab439
RaselMridha792 <raselmridha792@gmail.com>
2026-06-11 23:46:02 +0600

    problem 2 solutions

--- solutions.ts
+++ solutions.ts
@@ -4,3 +4,7 @@ function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
 
+// Problem 2 solution
+function reverseString(input: string): string {
+  return input.split("").reverse().join("");
+}
